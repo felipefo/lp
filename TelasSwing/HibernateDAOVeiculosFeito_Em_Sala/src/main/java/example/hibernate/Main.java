c7cd2859126c9355d5c865e94e71da5eb440871c
felipefo <felipefo@gmail.com>
2017-05-19 14:51:05 -0300

    .
    .

---
+++ TelasSwing/HibernateDAOVeiculosFeito_Em_Sala/src/main/java/example/hibernate/Main.java
@@ -0,0 +1,15 @@
+package example.hibernate;
+
+import example.hibernate.visao.TelaPrincipal;
+
+
+public class Main {    		
+	public static void main(String[] args) throws Exception {                                    
+                        
+            
+           TelaPrincipal main = new TelaPrincipal();
+           main.setVisible(true);
+           
+            
+	}                
+}
